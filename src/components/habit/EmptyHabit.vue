5f847dd95e91c28ce55ff514858475ca107ba8bd
George Shkurta <g.shkurta@takt.gr>
2025-01-28 10:10:14 +0200

    Add EmptyHabit component and integrate lucide-vue-next icons

---
+++ src/components/habit/EmptyHabit.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Plus } from 'lucide-vue-next';
+
+</script>
+
+<template>
+    <div class="border-2 border-dashed border-gray-200 rounded-xl flex items-center justify-center h-56">
+        <div class="text-gray-600 flex items-center">
+            Click the <Plus :size="18" class="text-black mx-1.5" /> button to create a new habit.
+        </div>
+    </div>
+</template>
