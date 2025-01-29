aeb7e8b4c02bb9ca7966050075723cda4addda95
George Shkurta <g.shkurta@takt.gr>
2025-01-29 09:44:51 +0200

    Refactor ToolBar component to use useColorMode for theme management

--- src/components/common/ToolBar.vue
+++ src/components/common/ToolBar.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { useColorMode } from "@vueuse/core";
 
 import { Button } from '@/components/ui/button';
 import { Sun, Moon, SunMoon } from 'lucide-vue-next';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
-const theme = ref('auto');
 const mode = useColorMode();
 </script>
 
@@ -16,15 +14,14 @@ const mode = useColorMode();
         <DropdownMenu>
             <DropdownMenuTrigger as-child>
                 <Button variant="ghost" size="icon">
-                    <Sun v-if="theme === 'light'" />
-                    <Moon v-else-if="theme === 'dark'" />
+                    <Sun v-if="mode === 'light'" />
+                    <Moon v-else-if="mode === 'dark'" />
                     <SunMoon v-else />
                     <span class="sr-only">Toggle theme</span>
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent :align="'end'">
-                <DropdownMenuRadioGroup v-model="theme" @update:model-value="mode = theme">
-                    <DropdownMenuRadioItem value="auto">Auto</DropdownMenuRadioItem>
+                <DropdownMenuRadioGroup v-model="mode">
                     <DropdownMenuRadioItem value="light">Light</DropdownMenuRadioItem>
                     <DropdownMenuRadioItem value="dark">Dark</DropdownMenuRadioItem>
                 </DropdownMenuRadioGroup>
