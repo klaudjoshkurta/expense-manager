38d611c112f0aaf3b8d8fd8af61ca9cd57a5df69
George Shkurta <g.shkurta@takt.gr>
2025-01-29 14:46:21 +0200

    Remove unused files and configurations to clean up the project

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import ToolBar from '@/components/common/ToolBar.vue';
-</script>
-
-<template>
-    <div class="h-screen z-0">
-        <ToolBar />
-    </div>
-</template>
