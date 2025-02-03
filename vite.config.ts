fbd4f29b2b5f6333d380102f00723e45a3400fd3
George Shkurta <g.shkurta@takt.gr>
2025-02-03 08:16:45 +0200

    Add unplugin-fonts for Google Fonts integration in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import autoprefixer from 'autoprefixer'
 import tailwind from 'tailwindcss'
+import unfonts from 'unplugin-fonts/vite'
 
 // import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -15,6 +16,13 @@ export default defineConfig({
     },
     plugins: [
         vue(),
+        unfonts({
+            google: {
+                families: [
+                    { name: 'Inter', styles: 'ital,opsz,wght@0,14..32,100..900;1,14..32,100..900' }
+                ]
+            }
+        })
     ],
     resolve: {
         alias: {
