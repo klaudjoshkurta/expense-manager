05aadd87250eba7b3d5b11556dd7b641bad6f0a2
George Shkurta <g.shkurta@takt.gr>
2025-02-03 08:14:51 +0200

    Integrate Tailwind CSS and add utility functions for class management

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,23 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 
-// https://vite.dev/config/
+// import vueDevTools from 'vite-plugin-vue-devtools'
+
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    css: {
+        postcss: {
+            plugins: [tailwind(), autoprefixer()],
+        },
     },
-  },
+    plugins: [
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+    },
 })
